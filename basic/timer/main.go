dfa03ba070d60c3c3f16d5ec6679fb7dcdcc6c4b
lazzyq <zengqiang96@gmail.com>
2020-07-02 20:27:14 +0800

    timer

---
+++ basic/timer/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// 初始化2s的ticker
+	ticker := time.NewTicker(2 * time.Second)
+
+	for t := range ticker.C {
+		// 每2s执行一次
+		fmt.Println("now: ", t)
+	}
+}
+
+func resetTimer() {
+	timer := time.NewTimer(2 * time.Second)
+	fmt.Println("now1: ", time.Now())
+	// sleep一段时间，但是没有超过定时器的时间
+	time.Sleep(time.Second)
+	fmt.Println("now2: ", time.Now())
+	//  过了1s后，定时器还没有超时，此时重置定时器的时间为2s
+	if timer.Reset(2 * time.Second) {
+		select {
+		case t := <-timer.C:
+			// now2后2s才会执行
+			fmt.Println("timeout: ", t)
+		}
+	}
+}
+
+func normalTimer() {
+	// 设置2s的定时器
+	timer := time.NewTimer(2 * time.Second)
+	fmt.Println("now1: ", time.Now())
+	select {
+	case t := <-timer.C:
+		// now1后2s后会执行
+		fmt.Println("timeout: ", t)
+	}
+}
